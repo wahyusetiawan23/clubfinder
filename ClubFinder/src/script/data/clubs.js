22ab7be09efec4cc6c99e1df794f598a94544142
wahyusetiawan23 <wahyusetiawan.ws33@gmail.com>
2020-08-20 01:54:15 +0700

    first commit

---
+++ ClubFinder/src/script/data/clubs.js
@@ -0,0 +1,33 @@
+const clubs = [{
+        name: "Gunung Ciremai",
+        stadium: "Majalengka, Kuningan",
+        fanArt: "https://blue.kumparan.com/image/upload/fl_progressive,fl_lossy,c_fill,q_auto:best,w_640/v1562162254/kemyrfembstmktd5oufp.jpg",
+
+        description: "Nama gunung ini berasal dari kata cereme (Phyllanthus acidus, sejenis tumbuhan perdu berbuah kecil dengan rasa masam), namun sering kali disebut Ciremai, suatu gejala hiperkorek akibat banyaknya nama tempat di wilayah Pasundan yang menggunakan awalan 'ci-' untuk penamaan tempat. <br> Gunung Ceremai (sering kali secara salah kaprah dinamakan 'Ciremai') (Aksara Sunda Baku: ᮌᮥᮔᮥᮀ ᮎᮨᮛᮨᮙᮦ, Latin: Gunung Ceremé) adalah gunung berapi kerucut yang secara administratif termasuk dalam wilayah dua kabupaten, yakni Kabupaten Kuningan dan Kabupaten Majalengka, Provinsi Jawa Barat. Posisi geografis puncaknya terletak pada 6° 53' 30 LS dan 108° 24' 00 BT, dengan ketinggian 3.078 m di atas permukaan laut. Gunung ini merupakan gunung tertinggi di Jawa Barat."
+    },
+    {
+        name: "Gunung Parangrao",
+        stadium: "Cianjur,Sukabumi",
+        fanArt: "https://mytrip.co.id/images/uploads/29-08-19_16.46.008527.jpg",
+        description: "Gunung Pangrango (Aksara Sunda Baku: ᮌᮥᮔᮥᮀ ᮕᮍᮢᮍᮧ) merupakan sebuah gunung yang terdapat di pulau Jawa, Indonesia. Gunung Pangrango mempunyai ketinggian setinggi 3.019 meter dari permukaan laut. Puncaknya dinamakan Puncak Mandalawangi. Puncak Mandalawangi juga merupakan titik pertemuan batas tiga kabupaten yaitu Kabupaten Bogor, Kabupaten Cianjur dan Kabupaten Sukabumi.Gunung Pangrango merupakan gunung tertinggi kedua di Jawa Barat setelah Gunung Ceremai. Gunung Pangrango terletak persis bersebelahan dengan Gunung Gede dan berada dalam kawasan Taman Nasional Gede Pangrango. Gunung Pangrango mempunyai kawasan hutan Dipterokarp Bukit, hutan Dipterokarp Atas, hutan Montane, dan Hutan Ericaceous atau hutan gunung."
+    },
+    {
+        name: "Gunung Gede",
+        stadium: "Cianjur, Sukabumi",
+        fanArt: "https://images.pojoksatu.id/2019/06/Gunung-Gede.jpg",
+        description: "Gunung Gede (Aksara Sunda Baku: ᮌᮥᮔᮥᮀ ᮌᮨᮓᮦ, Gunung Gedé) merupakan sebuah gunung api bertipe stratovolcano yang berada di Pulau Jawa, Indonesia. Gunung Gede berada dalam ruang lingkup Taman Nasional Gede Pangrango, yang merupakan salah satu dari lima taman nasional yang pertama kali diumumkan di Indonesia pada tahun 1980. Gunung ini berada di dua wilayah kabupaten yaitu Kabupaten Cianjur dan Sukabumi, dengan ketinggian 1.000 - 2.958 m. dpl, dan berada pada lintang 106°51' - 107°02' BT dan 64°1' - 65°1 LS. Suhu rata-rata di puncak gunung Gede 18 °C dan di malam hari suhu puncak berkisar 5 °C, dengan curah hujan rata-rata 3.600 mm/tahun. Gerbang utama menuju gunung ini adalah dari Cibodas dan Cipanas.",
+    },
+    {
+        name: "Gunung Cikuray",
+        stadium: "Garut",
+        fanArt: "https://2.bp.blogspot.com/-ceJ73QeG-tQ/Vs_S6hT-OwI/AAAAAAAAHsU/jvaDD1Tv2pI/s640/cikuray.jpg",
+        description: "Gunung Cikuray atau Cikurai (Aksara Sunda Baku: ᮌᮥᮔᮥᮀ ᮎᮤᮊᮥᮛᮄ) adalah sebuah gunung bertipe Stratovolcano yang terletak di Dayeuhmanggung, Kabupaten Garut, Jawa Barat, Indonesia. Gunung Cikuray yang mempunyai ketinggian 2.821 meter di atas permukaan laut ini tidak mempunyai kawah aktif dan merupakan gunung tertinggi keempat di Jawa Barat setelah Gunung Ciremai, Gunung Pangrango dan Gunung Gede. Gunung ini berada di perbatasan kecamatan Bayongbong dari sini bisa naik melalui jalur Cilegug atau kampung Jambansari dekat markas HdG Team, Cikajang, Kiara Janggot dan Dayeuhmanggung. Iklim di daerah Gunung Cikuray dan sekitarnya dikategorikan sebagai daerah beriklim tropis basah (humid tropical climate). Curah hujan di sekitar Gunung Cikuray mencapai 3500-4000mm dengan kalkulasi bulan basah 9 bulan dan bulan kering 3 bulan dan juga variasi temperatur dari 10º C hingga 24º C.",
+    },
+    {
+        name: "Gunung Papandayan",
+        stadium: "Garut",
+        fanArt: "https://www.tagar.id/Asset/uploads2019/1582384190818-gunung-papandayan-garut.jpg",
+        description: "Gunung Papandayan (Aksara Sunda Baku: ᮌᮥᮔᮥᮀ ᮕᮕᮔ᮪ᮓᮚᮔ᮪) adalah gunung api strato[2] yang terletak di Kabupaten Garut, Jawa Barat tepatnya di Kecamatan Cisurupan. Gunung dengan ketinggian 2665 meter di atas permukaan laut itu terletak sekitar 70 km sebelah tenggara Kota Bandung. Pada Gunung Papandayan, terdapat beberapa kawah yang terkenal. Di antaranya Kawah Mas, Kawah Baru, Kawah Nangklak, dan Kawah Manuk. Kawah-kawah tersebut mengeluarkan uap dari sisi dalamnya.Topografi di dalam kawasan curam, berbukit dan bergunung serta terdapat tebing yang terjal. Menurut kalisifikasi Schmidt dan Ferguson termasuk type iklim B, dengan curah hujan rata-rata 3.000 mm/thn, kelembaban udara 70 – 80 % dan temperatur 10 °C."
+    }
+];
+export default clubs;
